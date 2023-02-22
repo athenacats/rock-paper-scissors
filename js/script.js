3d924ba0ae8274c295676571e0ba5e40575e6753
athenacats <124017569+athenacats@users.noreply.github.com>
2023-02-22 21:36:15 +0300

    New function roundOne not working

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 function getComputerChoice(rock, paper, scissors) {
-    let playerSelection = prompt("Choose rock, paper or scissors");
-    let computerSelection = paper;
+    function roundOne (playerSelection = prompt("Choose rock, paper or scissors"),computerSelection = paper)
     if (playerSelection.toLowerCase == rock) {
         return "You lose! Paper trumps Rock"
     }
@@ -10,5 +9,4 @@ function getComputerChoice(rock, paper, scissors) {
     if (playerSelection.toLowerCase == scissors) {
         return "You win! Scissors trump Paper!"
     }
-    console.log(getComputerChoice)
 }
