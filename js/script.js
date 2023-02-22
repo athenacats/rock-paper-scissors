9e67785f9c71e439a47395999de2b74a42cbe1c1
athenacats <124017569+athenacats@users.noreply.github.com>
2023-02-22 21:13:46 +0300

    INput initial thoughts on Function syntax

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-function getComputerChoice
+function getComputerChoice(rock, paper, scissors) {
+    let playerSelection = prompt("Choose rock, paper or scissors");
+    let computerSelection = paper;
+    if (playerSelection.toLowerCase == rock) {
+        return "You lose! Paper trumps Rock"
+    }
+    if (playerSelection.toLowerCase == paper) {
+        return "Draw! Go again!"
+    }
+    if (playerSelection.toLowerCase == scissors) {
+        return "You win! Scissors trump Paper!"
+    }
+    console.log(getComputerChoice)
+}
