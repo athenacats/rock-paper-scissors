47dae6ff5d3c94ca167032c91207f1e86e7776aa
athenacats <124017569+athenacats@users.noreply.github.com>
2023-02-23 13:51:05 +0300

    Change arrangement of functions and let, not working

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
-const getComputerChoice = ('rock', 'paper', 'scissors')
+let getComputerChoice = ["rock", "paper", "scissors"]
 let computerSelection = getComputerChoice[Math.floor(Math.random()*getComputerChoice.length)] 
 let playerSelection = prompt("Choose Rock, Paper or Scissors")
 function roundOne(playerSelection, computerSelection) {
-        if (playerSelection.toLowerCase == computerSelection) {
+        if (playerSelection.toLowerCase() === computerSelection) {
             return "Draw!"
         }
     
-        else if (playerSelection.toLowerCase == "paper" &&
+        else if (playerSelection.toLowerCase() === "paper" &&
             computerSelection == "rock") {
             return "You win!"
         }
-        else if (playerSelection.toLowerCase == "rock" &&
+        else if (playerSelection.toLowerCase() === "rock" &&
             computerSelection == "scissors") {
             return "You win!"
         }
-        else if (playerSelection == "scissors" &&
+        else if (playerSelection.toLowerCase() === "scissors" &&
             computerSelection == "paper") {
                 return "You win!"
             }
