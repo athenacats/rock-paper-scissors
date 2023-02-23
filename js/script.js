04216af1d59bece347858cfd63dfd03ab746e3a5
athenacats <124017569+athenacats@users.noreply.github.com>
2023-02-23 13:59:02 +0300

    Add more else if conditions

--- js/script.js
+++ js/script.js
@@ -3,23 +3,36 @@ let computerSelection = getComputerChoice[Math.floor(Math.random()*getComputerCh
 let playerSelection = prompt("Choose Rock, Paper or Scissors")
 function roundOne(playerSelection, computerSelection) {
         if (playerSelection.toLowerCase() === computerSelection) {
-            return "Draw!"
+            return "Draw! " + playerSelection + " is the same as " + computerSelection
         }
     
         else if (playerSelection.toLowerCase() === "paper" &&
-            computerSelection == "rock") {
-            return "You win!"
+            computerSelection === "rock") {
+            return "You win! " + playerSelection + " trumps " + computerSelection
         }
         else if (playerSelection.toLowerCase() === "rock" &&
-            computerSelection == "scissors") {
-            return "You win!"
+            computerSelection === "scissors") {
+            return "You win! " + playerSelection + " trumps " + computerSelection
         }
         else if (playerSelection.toLowerCase() === "scissors" &&
-            computerSelection == "paper") {
-                return "You win!"
+            computerSelection === "paper") {
+                return "You win! " + playerSelection + " trumps " + computerSelection
             }
-        else {
-            return "You lose!"
+        else if (playerSelection.toLowerCase() === "paper" &&
+        computerSelection === "scissors") {
+            return "You lose! " + computerSelection + " trumps " + playerSelection
         }
+        else if (playerSelection.toLowerCase() === "rock" &&
+        computerSelection === "paper") {
+            return "You lose! " + computerSelection + " trumps " + playerSelection
+        }
+        else if (playerSelection.toLowerCase() === "scissors" &&
+        computerSelection === "rock") {
+            return "You lose! " + computerSelection + " trumps " + playerSelection
+        }
+        else {
+            return "You must make a selection"
+        }
+
     }
 
