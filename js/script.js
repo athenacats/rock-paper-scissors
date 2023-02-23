18479913cf89d55a28966b3870de615bf6149a29
athenacats <124017569+athenacats@users.noreply.github.com>
2023-02-23 17:36:42 +0300

    Introduce game function, fail

--- js/script.js
+++ js/script.js
@@ -9,43 +9,44 @@ console.log(computerSelection)
 let playerSelection = prompt("Choose Rock, Paper or Scissors")
 
 for (let i = 0; i <= 5; i++) {
-    function 
-}
-
-function roundOne(playerSelection, computerSelection) {
-        if (playerSelection.toLowerCase() === computerSelection) {
-            return "Draw!";
-        }
+    function game(i){ 
+        function roundOne(playerSelection, computerSelection) {
+            if (playerSelection.toLowerCase() === computerSelection) {
+                return "Draw!";
+            }
     
-        else if (playerSelection.toLowerCase() === "paper" &&
+            else if (playerSelection.toLowerCase() === "paper" &&
             computerSelection === "rock") {
-            return "You win!";
-        }
-        else if (playerSelection.toLowerCase() === "rock" &&
+                return "You win!";
+            }
+            else if (playerSelection.toLowerCase() === "rock" &&
             computerSelection === "scissors") {
-            return "You win!";
-        }
-        else if (playerSelection.toLowerCase() === "scissors" &&
+                return "You win!";
+            }
+            else if (playerSelection.toLowerCase() === "scissors" &&
             computerSelection === "paper") {
                 return "You win!";
             }
-        else if (playerSelection.toLowerCase() === "paper" &&
-        computerSelection === "scissors") {
-            return "You lose!";
+            else if (playerSelection.toLowerCase() === "paper" &&
+            computerSelection === "scissors") {
+                return "You lose!";
+            }
+            else if (playerSelection.toLowerCase() === "rock" &&
+            computerSelection === "paper") {
+                return "You lose!";
+            }
+            else if (playerSelection.toLowerCase() === "scissors" &&
+            computerSelection === "rock") {
+                return "You lose!";
+            }
+            else {
+                return "You must make a selection";
+            }
         }
-        else if (playerSelection.toLowerCase() === "rock" &&
-        computerSelection === "paper") {
-            return "You lose!";
-        }
-        else if (playerSelection.toLowerCase() === "scissors" &&
-        computerSelection === "rock") {
-            return "You lose!";
-        }
-        else {
-            return "You must make a selection";
-        }
+        console.log(roundOne(playerSelection, computerSelection))
     }
+}
 
     
-    console.log(roundOne(playerSelection, computerSelection))
+   
 
