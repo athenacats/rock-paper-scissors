a1650585dec7020160e830e1182a91de8952b9e8
athenacats <124017569+athenacats@users.noreply.github.com>
2023-02-23 10:49:23 +0300

    Introduce parameters 'choice', 'roundOne'
    write down preliminary thoughts on loop for if..else statements

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
-function getComputerChoice(rock, paper, scissors) {
-    function roundOne (playerSelection = prompt("Choose rock, paper or scissors"),computerSelection = paper)
-    if (playerSelection.toLowerCase == rock) {
-        return "You lose! Paper trumps Rock"
+function getComputerChoice(choice) 
+    let choice = ('rock','paper','scissors');
+    let playerSelection = prompt("Choose rock, paper or scissors");
+    let computerSelection = choice[Math.floor(Math.random()*choice.length)]
+    function roundOne(playerSelection, computerSelection) {
+        if (playerSelection.toLowerCase === computerSelection) {
+            return "Draw!"
+        }
+        else if (playerSelection.toLowerCase == paper &&
+            computerSelection == rock) {
+            return "You win!"
+        }
+        else if (playerSelection.toLowerCase == rock &&
+            computerSelection == scissors) {
+            return "You win!"
+        }
+        else if (playerSelection == scissors &&
+            computerSelection == paper) {
+                return "You win!"
+            }
+        else {
+            return "You lose!"
+        }
     }
-    if (playerSelection.toLowerCase == paper) {
-        return "Draw! Go again!"
-    }
-    if (playerSelection.toLowerCase == scissors) {
-        return "You win! Scissors trump Paper!"
-    }
-}
