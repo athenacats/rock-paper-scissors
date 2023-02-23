1dd59476fb061638a9647d4b6103ba1a655ebdb3
athenacats <124017569+athenacats@users.noreply.github.com>
2023-02-23 12:04:29 +0300

    Add quotation to else if strings, not working

--- js/script.js
+++ js/script.js
@@ -1,21 +1,21 @@
 const getComputerChoice = ('rock', 'paper', 'scissors')
 let computerSelection = getComputerChoice[Math.floor(Math.random()*getComputerChoice.length)] 
 let playerSelection = prompt("Choose Rock, Paper or Scissors")
-    function roundOne(playerSelection, computerSelection) {
-        if (playerSelection.toLowerCase === computerSelection) {
+function roundOne(playerSelection, computerSelection) {
+        if (playerSelection.toLowerCase == computerSelection) {
             return "Draw!"
         }
     
-        else if (playerSelection.toLowerCase == paper &&
-            computerSelection == rock) {
+        else if (playerSelection.toLowerCase == "paper" &&
+            computerSelection == "rock") {
             return "You win!"
         }
-        else if (playerSelection.toLowerCase == rock &&
-            computerSelection == scissors) {
+        else if (playerSelection.toLowerCase == "rock" &&
+            computerSelection == "scissors") {
             return "You win!"
         }
-        else if (playerSelection == scissors &&
-            computerSelection == paper) {
+        else if (playerSelection == "scissors" &&
+            computerSelection == "paper") {
                 return "You win!"
             }
         else {
