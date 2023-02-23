1d4b7133820c5ce31704b361499dc76850a8528c
athenacats <124017569+athenacats@users.noreply.github.com>
2023-02-23 14:13:53 +0300

    Elaborate on return statements. Output not working

--- js/script.js
+++ js/script.js
@@ -3,35 +3,35 @@ let computerSelection = getComputerChoice[Math.floor(Math.random()*getComputerCh
 let playerSelection = prompt("Choose Rock, Paper or Scissors")
 function roundOne(playerSelection, computerSelection) {
         if (playerSelection.toLowerCase() === computerSelection) {
-            return "Draw! " + playerSelection + " is the same as " + computerSelection
+            return "Draw! " + playerSelection + " is the same as " + computerSelection;
         }
     
         else if (playerSelection.toLowerCase() === "paper" &&
             computerSelection === "rock") {
-            return "You win! " + playerSelection + " trumps " + computerSelection
+            return "You win! " + playerSelection + " trumps " + computerSelection;
         }
         else if (playerSelection.toLowerCase() === "rock" &&
             computerSelection === "scissors") {
-            return "You win! " + playerSelection + " trumps " + computerSelection
+            return "You win! " + playerSelection + " trumps " + computerSelection;
         }
         else if (playerSelection.toLowerCase() === "scissors" &&
             computerSelection === "paper") {
-                return "You win! " + playerSelection + " trumps " + computerSelection
+                return "You win! " + playerSelection + " trumps " + computerSelection;
             }
         else if (playerSelection.toLowerCase() === "paper" &&
         computerSelection === "scissors") {
-            return "You lose! " + computerSelection + " trumps " + playerSelection
+            return "You lose! " + computerSelection + " trumps " + playerSelection;
         }
         else if (playerSelection.toLowerCase() === "rock" &&
         computerSelection === "paper") {
-            return "You lose! " + computerSelection + " trumps " + playerSelection
+            return "You lose! " + computerSelection + " trumps " + playerSelection;
         }
         else if (playerSelection.toLowerCase() === "scissors" &&
         computerSelection === "rock") {
-            return "You lose! " + computerSelection + " trumps " + playerSelection
+            return "You lose! " + computerSelection + " trumps " + playerSelection;
         }
         else {
-            return "You must make a selection"
+            return "You must make a selection";
         }
 
     }
