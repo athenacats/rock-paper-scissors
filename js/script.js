79efb210ec0f2f37349f2c6471a8dcba0a28f72e
athenacats <124017569+athenacats@users.noreply.github.com>
2023-03-06 15:31:17 +0300

    Bug still not fixed

--- js/script.js
+++ js/script.js
@@ -8,9 +8,10 @@ const game = () => {
     const playGame = () => {
         const rockBtn = document.querySelector('.rock');
         const paperBtn = document.querySelector('.paper');
-        const scissorBtn = document.querySelector('.scissors');
+        const scissorBtn = document.querySelector('.scissor');
         const playerOptions = [rockBtn, paperBtn, scissorBtn];
-        const computerOptions = ['rock', 'paper', 'scissors'];
+        const computerOptions = ['rock', 'paper', 'scissors']
+        
     
 
 
@@ -18,104 +19,99 @@ const game = () => {
             option.addEventListener('click', function(){
                 const movesLeft = document.querySelector('.movesleft');
                 moves++;
-                movesLeft.innerText = `Hatua Zilizobaki (Moves Left): ${10 - moves}`;
-
-                const computerChoice = computerOptions[Math.floor(Math.random()*3)];
+                movesLeft.innerText = `Hatua Zilizobaki (Moves Left): ${10-moves}`;
+                
+                const choiceNumber = Math.floor(Math.random()*3);
+                const computerChoice = computerOptions[choiceNumber];
  
-                winner(this.innerText.toLowerCase(), computerChoice.toLowerCase());
+                winner(this.innerText, computerChoice);
 
                 if (moves == 10){
                     gameOver(playerOptions,movesLeft);//
                 }
-            });
-        
-        });
-
+            })
+            
+        })
     }
-        const winner = (player, computer) => {
-            const result = document.querySelector('.result');
-            const playerScoreBoard = document.querySelector('.playerCounter');
-            const computerScoreBoard = document.querySelector('.computerCounter');
-            //player = player.toLowerCase();
-            //computer = computerChoice.toLowerCase();
+    const winner = (player, computer) => {
+        const result = document.querySelector('.result');
+        const playerScoreBoard = document.querySelector('.playerCounter');
+        const computerScoreBoard = document.querySelector('.computerCounter');
+        player = player.toLowerCase();
+        computer = computer.toLowerCase();
 
-            if (player === computer) {
-                result.textContent = 'It is a Draw!';
-            }
-            
-            else if (player == "paper" &&
-            computer == "rock") {
+        if (player === computer) {
+            result.textContent = 'It is a Draw!';
+        }
+        
+        else if (player == "rock") {
+            if (computer == 'scissors'){
                 result.textContent = 'You Win!';
                 playerScore++;
                 playerScoreBoard.textContent = playerScore;
-            
-            }
-            else if (player == "rock" &&
-            computer == "scissors") {
-                result.textContent = 'You Win!';
-                playerScore++;
-                playerScoreBoard.textContent = playerScore;
-            }
-            else if (player == "scissors" &&
-            computer == "paper") {
-                result.textContent = 'You Win!';
-                playerScore++;
-                playerScoreBoard.textContent = playerScore;
-            }
-            else if (player == "paper" &&
-            computer == "scissors") {
+            }else{
                 result.textContent = 'You Lose!';
                 computerScore++;
                 computerScoreBoard.textContent = computerScore;
             }
-            else if (player == "rock" &&
-            computer == "paper") {
+        } else if (player == "scissors") {
+            if (computer == "paper"){
+                result.textContent = 'You Win!';
+                playerScore++;
+                playerScoreBoard.textContent = playerScore;
+            }else{
                 result.textContent = 'You Lose!';
                 computerScore++;  
                 computerScoreBoard.textContent = computerScore;
             }
-            else if (player == "scissors" &&
-            computer== "rock") {
+        }else if (player == 'paper') {
+            if (computer == 'rock'){
+                result.textContent = 'You Win!';
+                playerScore++;
+                playerScoreBoard.textContent = playerScore;
+            }else{
                 result.textContent = 'You Lose!';
                 computerScore++;
                 computerScoreBoard.textContent = computerScore;
             }
-           
-        };
+        }
+        
+    };
+        
 
     
-    const gameOver =(playerOptions,movesLeft) => {
+    const gameOver = (playerOptions, movesLeft) => {
         const chooseMove = document.querySelector('.choose');
         const result = document.querySelector('.result');
         const reloadBtn= document.querySelector('.reload');
 
         playerOptions.forEach(option => {
             option.style.display = 'none';
-        });
+        })
 
-        chooseMove.textContent = 'Game Over!';
+        chooseMove.innerText = 'Game Over!'
         movesLeft.style.display = 'none';
 
         if (playerScore > computerScore) {
             result.style.fontSize = '24px';
-            result.textContent = "Congratulations, you win you lucky ducky!";
+            result.innerText = "Congratulations, you win you lucky ducky!";
         }
         else if (playerScore < computerScore) {
             result.style.fontSize = '24px';
-            result.textContent = "Waah, waah, you lose, try again!";
+            result.innerText = "Waah, waah, you lose, try again!";
             result.style.color = 'red';
         }
         else {
             result.style.fontSize = '24px';
-            result.textContent = "This game is a tie";
+            result.innerText = "This game is a tie";
         }
 
         reloadBtn.innerText = 'Restart';
         reloadBtn.style.display = 'flex';
         reloadBtn.addEventListener('click', () => {
             window.location.reload();
-        }) ;       
-    };
+        })    
+    }
 
     playGame();
     
