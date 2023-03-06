6dcdf42ffa131a56ea44518aaf241dba2866312c
athenacats <124017569+athenacats@users.noreply.github.com>
2023-03-06 07:44:14 +0300

    Add final script code
    Got stuck for a long time but forgot to commit. Will work on that habit

--- js/script.js
+++ js/script.js
@@ -1,71 +1,126 @@
-let choice = ["rock", "paper", "scissors"];
+const game = () => {
+    let playerScore = 0;
+    let computerScore = 0;
+    let moves = 0;
 
-function getComputerChoice() {
-    return choice[Math.floor(Math.random()*choice.length)]; 
+   
+
+    const playGame = () => {
+        const rockBtn = document.querySelector('.rock');
+        const paperBtn = document.querySelector('.paper');
+        const scissorBtn = document.querySelector('.scissors');
+        const playerOptions = [rockBtn, paperBtn, scissorBtn];
+        const computerOptions = ['rock', 'paper', 'scissors'];
+    
+
+
+        playerOptions.forEach(option => {
+            option.addEventListener('click', function(){
+                const movesLeft = document.querySelector('.movesLeft');
+                moves++;
+                movesLeft.innerText = `Hatua Zilizobaki (Moves Left): ${10-moves}`;
+
+                const computerChoice = computerOptions[Math.floor(Math.random()*3)];
+
+                winner(this.innerText,computerChoice)
+
+                if (moves == 10){
+                    gameOver(playerOptions,movesLeft);
+                }
+            })
+        
+        });
+    
+    }
+    
+
+    const winner = (player, computer) => {
+        const result = document.querySelector('.result');
+        const playerScoreBoard = document.querySelector('.playerCounter');
+        const computerScoreBoard = document.querySelector('.computerCounter');
+        player = player.toLowerCase();
+        computer = computer.toLowerCase();
+
+        if (player == computer) {
+            result.textContent = 'It is a Draw!'
+        }
+
+        else if (player == "paper" &&
+        computer == "rock") {
+            playerScore++;
+            result.textContent = 'You Win!'
+            playerScoreBoard.textContent = playerScore;
+        
+        }
+        else if (player== "rock" &&
+        computer == "scissors") {
+            playerScore++;
+            result.textContent = 'You Win!'
+            playerScoreBoard.textContent = playerScore;
+        }
+        else if (player == "scissors" &&
+        computer == "paper") {
+            playerScore++;
+            result.textContent = 'You Win!'
+            playerScoreBoard.textContent = playerScore;
+        }
+        else if (player == "paper" &&
+        computer == "scissors") {
+            computerScore++;
+            result.textContent = 'You Lose!'
+            computerScoreBoard.textContent = computerScore;
+        }
+        else if (player == "rock" &&
+        computer == "paper") {
+            computerScore++;
+            result.textContent = 'You Lose!'
+            computerScoreBoard.textContent = computerScore;
+        }
+        else if (player == "scissors" &&
+        computer== "rock") {
+            computerScore++;
+            result.textContent = 'You Lose!'
+            computerScoreBoard.textContent = computerScore;
+        }
+    }
+        
+    const gameOver =(playerOptions,movesLeft) => {
+        const chooseMove = document.querySelector('.choose');
+        const result = document.querySelector('.result');
+        const reloadBtn= document.querySelector('.reload');
+
+        playerOptions.forEach(option => {
+            option.style.display = 'none';
+        })
+
+        chooseMove.innerText = 'Game Over!'
+        movesLeft.style.display = 'none';
+
+        if (playerScore > computerScore) {
+            result.style.fontSize = '18px'
+            result.innerText = "Congratulations, you win you lucky ducky!";
+        }
+        else if (playerScore < computerScore) {
+            result.style.fontSize = '18px'
+            result.innerText = "Waah, waah, you lose, try again!";
+            result.style.color = 'red';
+        }
+        else {
+            result.style.fontSize = '18px';
+            result.innerText = "This game is a tie";
+        }
+
+        reloadBtn.innerText = 'Restart';
+        reloadBtn.style.display = 'flex';
+        reloadBtn.addEventListener('click', () => {
+            window.location.reload();
+        })        
     }
 
-let computerSelection = getComputerChoice()
-let playerSelection = prompt("Choose Rock, Paper or Scissors")
-console.log("You chose " + playerSelection)
-console.log("The computer chose " + computerSelection)
-alert("You chose " + playerSelection + " and the computer chose " + computerSelection)
-
-let computerScore = 0
-let playerScore = 0
-
-function roundOne(playerSelection, computerSelection) {
-    if (playerSelection.toLowerCase() === computerSelection) {
-        playerScore++;
-        computerScore++;
-        alert("Draw!");
-        return "Draw!";
-    }
-
-    else if (playerSelection.toLowerCase() === "paper" &&
-    computerSelection === "rock") {
-        playerScore++;
-        alert("You win!");
-        return "You win!";
-    }
-    else if (playerSelection.toLowerCase() === "rock" &&
-    computerSelection === "scissors") {
-        playerScore++;
-        alert("You win!");
-        return "You win!";
-    }
-    else if (playerSelection.toLowerCase() === "scissors" &&
-    computerSelection === "paper") {
-        playerScore++;
-        alert("You win!");
-        return "You win!";
-    }
-    else if (playerSelection.toLowerCase() === "paper" &&
-    computerSelection === "scissors") {
-        computerScore++;
-        alert("You lose!");
-        return "You lose!";
-    }
-    else if (playerSelection.toLowerCase() === "rock" &&
-    computerSelection === "paper") {
-        computerScore++;
-        alert("You lose!");
-        return "You lose!";
-    }
-    else if (playerSelection.toLowerCase() === "scissors" &&
-    computerSelection === "rock") {
-        computerScore++;
-        alert("You lose!");
-        return "You lose!";
-    }
-    else {
-        alert("You must make a selection");
-        return "You must make a selection";
-    }
+    playGame();
 }
+game();
 
-console.log(roundOne(playerSelection, computerSelection))
-console.log("Player Score is " + playerScore + " and Computer Score is " + computerScore + "\n\   ")
-alert("Player Score is " + playerScore + " and Computer Score is " + computerScore)
 
 
    
