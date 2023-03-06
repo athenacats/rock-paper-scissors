fa0c98abef8a4e623e378ccd249cbc4355252420
athenacats <124017569+athenacats@users.noreply.github.com>
2023-03-06 10:13:19 +0300

    Found bug, always returns the game is a tie

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ const game = () => {
         const paperBtn = document.querySelector('.paper');
         const scissorBtn = document.querySelector('.scissors');
         const playerOptions = [rockBtn, paperBtn, scissorBtn];
-        const computerOptions = ['rock', 'paper', 'scissors'];
+        //const computerOptions = ['rock', 'paper', 'scissors'];
     
 
 
@@ -18,72 +18,76 @@ const game = () => {
             option.addEventListener('click', function(){
                 const movesLeft = document.querySelector('.movesLeft');
                 moves++;
-                movesLeft.innerText = `Hatua Zilizobaki (Moves Left): ${10-moves}`;
+                movesLeft.innerText = `Hatua Zilizobaki (Moves Left): ${10 - moves}`;
 
-                const computerChoice = computerOptions[Math.floor(Math.random()*3)];
-
-                winner(this.innerText,computerChoice)
+                const computerChoice = ["rock", "paper", "scissors"][Math.floor(Math.random()*3)];
+ 
+                winner(this.innerText.toLowerCase(), computerChoice.toLowerCase);
 
                 if (moves == 10){
-                    gameOver(playerOptions,movesLeft);
+                    gameOver(playerOptions,movesLeft);//
                 }
-            })
+            });
         
         });
     
-    }
+    
     
 
-    const winner = (player, computer) => {
-        const result = document.querySelector('.result');
-        const playerScoreBoard = document.querySelector('.playerCounter');
-        const computerScoreBoard = document.querySelector('.computerCounter');
-        player = player.toLowerCase();
-        computer = computer.toLowerCase();
+        const winner = (player, computer) => {
+            const result = document.querySelector('.result');
+            const playerScoreBoard = document.querySelector('.playerCounter');
+            const computerScoreBoard = document.querySelector('.computerCounter');
+            //player = player.toLowerCase();
+            //computer = computerChoice.toLowerCase();
+            console.log();
 
-        if (player == computer) {
-            result.textContent = 'It is a Draw!'
-        }
-
-        else if (player == "paper" &&
-        computer == "rock") {
-            playerScore++;
-            result.textContent = 'You Win!'
-            playerScoreBoard.textContent = playerScore;
-        
-        }
-        else if (player== "rock" &&
-        computer == "scissors") {
-            playerScore++;
-            result.textContent = 'You Win!'
-            playerScoreBoard.textContent = playerScore;
-        }
-        else if (player == "scissors" &&
-        computer == "paper") {
-            playerScore++;
-            result.textContent = 'You Win!'
-            playerScoreBoard.textContent = playerScore;
-        }
-        else if (player == "paper" &&
-        computer == "scissors") {
-            computerScore++;
-            result.textContent = 'You Lose!'
-            computerScoreBoard.textContent = computerScore;
-        }
-        else if (player == "rock" &&
-        computer == "paper") {
-            computerScore++;
-            result.textContent = 'You Lose!'
-            computerScoreBoard.textContent = computerScore;
-        }
-        else if (player == "scissors" &&
-        computer== "rock") {
-            computerScore++;
-            result.textContent = 'You Lose!'
-            computerScoreBoard.textContent = computerScore;
-        }
-    }
-        
+            if (player == computer) {
+                result.textContent = 'It is a Draw!';
+            }
+            
+            else if (player == "paper" &&
+            computer == "rock") {
+                playerScore++;
+                result.textContent = 'You Win!';
+                playerScoreBoard.textContent = playerScore;
+            
+            }
+            else if (player == "rock" &&
+            computer == "scissors") {
+                playerScore++;
+                result.textContent = 'You Win!';
+                playerScoreBoard.textContent = playerScore;
+            }
+            else if (player == "scissors" &&
+            computer == "paper") {
+                playerScore++;
+                result.textContent = 'You Win!';
+                playerScoreBoard.textContent = playerScore;
+            }
+            else if (player == "paper" &&
+            computer == "scissors") {
+                computerScore++;
+                result.textContent = 'You Lose!';
+                computerScoreBoard.textContent = computerScore;
+            }
+            else if (player == "rock" &&
+            computer == "paper") {
+                computerScore++;
+                result.textContent = 'You Lose!';
+                computerScoreBoard.textContent = computerScore;
+            }
+            else if (player == "scissors" &&
+            computer== "rock") {
+                computerScore++;
+                result.textContent = 'You Lose!';
+                computerScoreBoard.textContent = computerScore;
+            }
+            console.log();
+        };
+        console.log();
+    };
+    
     const gameOver =(playerOptions,movesLeft) => {
         const chooseMove = document.querySelector('.choose');
         const result = document.querySelector('.result');
@@ -91,35 +95,37 @@ const game = () => {
 
         playerOptions.forEach(option => {
             option.style.display = 'none';
-        })
+        });
 
-        chooseMove.innerText = 'Game Over!'
+        chooseMove.textContent = 'Game Over!';
         movesLeft.style.display = 'none';
 
         if (playerScore > computerScore) {
-            result.style.fontSize = '18px'
-            result.innerText = "Congratulations, you win you lucky ducky!";
+            result.style.fontSize = '24px';
+            result.textContent = "Congratulations, you win you lucky ducky!";
         }
         else if (playerScore < computerScore) {
-            result.style.fontSize = '18px'
-            result.innerText = "Waah, waah, you lose, try again!";
+            result.style.fontSize = '24px';
+            result.textContent = "Waah, waah, you lose, try again!";
             result.style.color = 'red';
         }
         else {
-            result.style.fontSize = '18px';
-            result.innerText = "This game is a tie";
+            result.style.fontSize = '24px';
+            result.textContent = "This game is a tie";
         }
 
         reloadBtn.innerText = 'Restart';
         reloadBtn.style.display = 'flex';
         reloadBtn.addEventListener('click', () => {
             window.location.reload();
-        })        
-    }
+        }) ;       
+    };
 
     playGame();
-}
+    
+};
 game();
+ 
 
 
 
