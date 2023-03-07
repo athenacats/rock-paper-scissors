84aa687e13119408ab06dfb8c5cf52629756b103
athenacats <124017569+athenacats@users.noreply.github.com>
2023-03-07 13:09:38 +0300

    Add comments

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//function to include the entire system that will be called
 function game() {
 
     
@@ -7,18 +8,23 @@ function game() {
 
     function playGame() {
 
+        /*ensure the choices look exactly as what is written in the 
+        buttons otherwise playerSelection will not work*/
         let choice = ["mawe (rock)", "karatasi (paper)", "makasi (scissors)"];
 
+        /*had to include this because it kept bringing error that addeventlistener
+        under forEach was null*/
         window.onload=function() {
-        const rock = document.querySelector('.rock');
-        const paper = document.querySelector('.paper');
-        const scissor = document.querySelector('.scissor');
-        const playerOptions = [rock, paper,scissor]
+            const rock = document.querySelector('.rock');
+            const paper = document.querySelector('.paper');
+            const scissor = document.querySelector('.scissor');
+            const playerOptions = [rock, paper,scissor]
 
-        
+            /*this calls each of the three buttons that can be called*/   
             playerOptions.forEach(option => {
                 option.addEventListener('click',function(){
 
+                    //so for each button clicked, moves increase//
                     const movesLeft = document.querySelector('.movesleft');
                     moves++;
                     movesLeft.innerText = `Moves Left: ${10-moves}`;
@@ -27,6 +33,9 @@ function game() {
                         return choice[Math.floor(Math.random()*choice.length)]; 
                         }
                     const computerSelection = getComputerChoice()
+
+                    //this calls to the function above, ie, the click which 
+                    //is essentially playerSelection
                     // Function to check who wins
                     winner(this.innerText,computerSelection)
                     
@@ -39,81 +48,81 @@ function game() {
         
         
         
+            //function to play the loop for all outcomes
+            function winner(playerSelection, computerSelection) {
+                const playerScoreBoard = document.querySelector('.playerCounter');
+                const computerScoreBoard = document.querySelector('.computerCounter');
+                const result = document.querySelector('.result');
+                
+                //not necessary, for me to make sure it works
+                console.log(computerSelection);
+                console.log(playerSelection);
+                if (playerSelection.toLowerCase() === computerSelection) {
+                    return result.textContent=  "Draw!";
+                }
 
-        function winner(playerSelection, computerSelection) {
-            const playerScoreBoard = document.querySelector('.playerCounter');
-            const computerScoreBoard = document.querySelector('.computerCounter');
-            const result = document.querySelector('.result');
-            console.log(computerSelection);
-            console.log(playerSelection);
-            if (playerSelection.toLowerCase() === computerSelection) {
-                return result.textContent=  "Draw!";
-            }
-
-            else if (playerSelection.toLowerCase() === "karatasi (paper)" &&
-            computerSelection === "mawe (rock)") {
-                playerScore++;
-                playerScoreBoard.textContent = playerScore;
-                return result.textContent = "You win!";
+                else if (playerSelection.toLowerCase() === "karatasi (paper)" &&
+                computerSelection === "mawe (rock)") {
+                    playerScore++;
+                    playerScoreBoard.textContent = playerScore;
+                    return result.textContent = "You win!";
+                    
+                }
+                else if (playerSelection.toLowerCase() === "mawe (rock)" &&
+                computerSelection === "makasi (scissors)") {
+                    playerScore++;
+                    playerScoreBoard.textContent = playerScore;
+                    return result.textContent = "You win!";
+                }
+                else if (playerSelection.toLowerCase() === "makasi (scissors)" &&
+                computerSelection === "karatasi (paper)") {
+                    playerScore++;
+                    playerScoreBoard.textContent = playerScore;
+                    return result.textContent = "You win!";
+                }
+                else if (playerSelection.toLowerCase() === "karatasi (paper)" &&
+                computerSelection === "makasi (scissors)") {
+                    computerScore++;
+                    computerScoreBoard.textContent = computerScore;
+                    return result.textContent = "You lose!";
+                }
+                else if (playerSelection.toLowerCase() === "mawe (rock)" &&
+                computerSelection === "karatasi (paper)") {
+                    computerScore++;
+                    computerScoreBoard.textContent = computerScore;
+                    return result.textContent = "You lose!";
+                }
+                else if (playerSelection.toLowerCase() === "makasi (scissors)" &&
+                computerSelection === "mawe (rock)") {
+                    computerScore++;
+                    computerScoreBoard.textContent = computerScore;
+                    return result.textContent = "You lose!";
+                }
+                //this helped in realising what was going wrong
+                else {
+                    return result.textContent = "Something went wrong";
+                }
                 
             }
-            else if (playerSelection.toLowerCase() === "mawe (rock)" &&
-            computerSelection === "makasi (scissors)") {
-                playerScore++;
-                playerScoreBoard.textContent = playerScore;
-                return result.textContent = "You win!";
-            }
-            else if (playerSelection.toLowerCase() === "makasi (scissors)" &&
-            computerSelection === "karatasi (paper)") {
-                playerScore++;
-                playerScoreBoard.textContent = playerScore;
-                return result.textContent = "You win!";
-            }
-            else if (playerSelection.toLowerCase() === "karatasi (paper)" &&
-            computerSelection === "makasi (scissors)") {
-                computerScore++;
-                computerScoreBoard.textContent = computerScore;
-                return result.textContent = "You lose!";
-            }
-            else if (playerSelection.toLowerCase() === "mawe (rock)" &&
-            computerSelection === "karatasi (paper)") {
-                computerScore++;
-                computerScoreBoard.textContent = computerScore;
-                return result.textContent = "You lose!";
-            }
-            else if (playerSelection.toLowerCase() === "makasi (scissors)" &&
-            computerSelection === "mawe (rock)") {
-                computerScore++;
-                computerScoreBoard.textContent = computerScore;
-                return result.textContent = "You lose!";
-            }
-            else {
-                return result.textContent = "Something went wrong";
-            }
-            
-        }
-        return winner;
+            return winner;
 
-        
+            //function for the game ending
+            function gameOver () {
+                const chooseMove = document.querySelector('.choose');
+                const result = document.querySelector('.result');
+                const reloadBtn = document.querySelector('.reload');
+                const movesLeft = document.querySelector('.movesleft');
 
+                //hides the buttons
+                rock.style.display = 'none';
+                paper.style.display = 'none';
+                scissor.style.display = 'none';
 
-        
-
-        function gameOver () {
-            const chooseMove = document.querySelector('.choose');
-            const result = document.querySelector('.result');
-            const reloadBtn = document.querySelector('.reload');
-            const movesLeft = document.querySelector('.movesleft');
-
-            rock.style.display = 'none';
-            paper.style.display = 'none';
-            scissor.style.display = 'none';
-
-            
+                //changes choose weapon to game over, hides moves left
                 chooseMove.innerText = 'Game Over!!'
                 movesLeft.style.display = 'none';
             
-
+                //what to show in the end after moves are over
                 if(playerScore > computerScore){
                     result.style.fontSize = '4vw';
                     result.innerText = 'Umeshinda! You Won The Game!'
@@ -129,56 +138,17 @@ function game() {
                     result.innerText = "Rudia! It's a Tie :|";
                     result.style.color = 'dark-grey'
                 }
+                //to display the reload button
                 reloadBtn.innerText = 'Restart';
                 reloadBtn.style.display = 'flex'
                 reloadBtn.addEventListener('click',() => {
                     window.location.reload();
-
-            
-        })
+                 })
         
-        return gameOver;
+            return gameOver;
+            }
         }
-    }
     }
     playGame();
 }
 game();
-
-
-/*console.log(roundOne(playerSelection, computerSelection))
-console.log("Player Score is " + playerScore + " and Computer Score is " + computerScore + "\n\   ")
-alert("Player Score is " + playerScore + " and Computer Score is " + computerScore)
-
-function game() {
-    for (i = 0; i < 4; i++) {
-        let choice = ["rock", "paper", "scissors"];
-
-        function getComputerChoice() {
-            return choice[Math.floor(Math.random()*choice.length)]; 
-        }
-
-        let computerSelection = getComputerChoice()
-        let playerSelection = prompt("Choose Rock, Paper or Scissors")
-        console.log("You chose " + playerSelection)
-        console.log("The computer chose " + computerSelection)
-        alert("You chose " + playerSelection + " and the computer chose " + computerSelection)
-        console.log(roundOne(playerSelection, computerSelection))
-        console.log("Player Score is " + playerScore + " and Computer Score is " + computerScore + "\n\   ")
-        alert("Player Score is " + playerScore + " and Computer Score is " + computerScore)
-        }
-        if (playerScore > computerScore) {
-            alert("Congratulations, you win you lucky ducky!");
-            return "Congratulations, you win you lucky ducky!";
-        }
-        else if (playerScore < computerScore) {
-            alert("Waah, waah, you lose, try again!");
-            return "Waah, waah, you lose, try again!";
-        }
-        else if (playerScore === computerScore) {
-            alert("This game is a tie");
-            return "This game is a tie";
-        }
-    }
-
-    console.log(game());*/
